c58085b986471a2472a320b8ce078aa89ff43bc6
Surakarn Samkaew <tonkla@gmail.com>
2019-12-23 22:15:56 +0700

    Add dotenv

--- api/main.go
+++ api/main.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 
-	"onepen/api/pkg/auth"
 	"onepen/api/pkg/graphql"
 )
 
+func init() {
+	var err error
+	if env := os.Getenv("ENV"); env != "" {
+		err = godotenv.Load(fmt.Sprintf(".env.%s", env))
+	} else {
+		err = godotenv.Load()
+	}
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
 	router := httprouter.New()
-	router.POST("/login", auth.Handle)
-	router.POST("/graphql", graphql.Handle)
+	router.POST("/graphql", graphql.Handler)
 	router.GET("/ping", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		fmt.Fprintln(w, "pong")
 	})
