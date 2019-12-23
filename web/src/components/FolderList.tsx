671970d35259c5f16c18492c02702c7f5c62401e
Surakarn Samkaew <tonkla@gmail.com>
2019-12-23 22:05:40 +0700

    npm up

--- web/src/components/FolderList.tsx
+++ web/src/components/FolderList.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import shortid from 'shortid'
 
 import Folder from './Folder'
 
 import '../styles/FolderList.scss'
 
+const addFolder = () => {
+  const id = shortid.generate()
+  console.log(id)
+}
+
 const FolderList = () => {
   return (
     <div className="folder-list">
       <h2>Folders</h2>
-      <div>Create Folder</div>
+      <div onClick={addFolder}>Add Folder</div>
       <ul>
         <Folder />
       </ul>
--- web/src/components/NoteList.tsx
+++ web/src/components/NoteList.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import shortid from 'shortid'
 
 import Note from './Note'
 
 import '../styles/NoteList.scss'
 
+const addNote = () => {
+  const id = shortid.generate()
+  console.log(id)
+}
+
 const NoteList = () => {
   return (
     <div className="note-list">
       <h2>Notes</h2>
-      <div>Create Note</div>
+      <div onClick={addNote}>Add Note</div>
       <ul>
         <Note />
       </ul>
